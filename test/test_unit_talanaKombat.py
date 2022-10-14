abd8c603769d51fd6e5ca1f2a3581f3f5157ca3a
polirritmico <ebray187@gmail.com>
2022-10-14 17:12:55 -0300

    Added test_load_json_basic

--- test/test_unit_talanaKombat.py
+++ test/test_unit_talanaKombat.py
@@ -12,9 +12,18 @@ class TestRepository(unittest.TestCase):
 
 
     #@unittest.skip
-    def test_dummy(self):
-        expected = "Expected output"
-        output = self.talanaKombat.a_function()
-        self.assertEqual(expected, output)
+    def test_load_json_basic(self):
+        file_path = "test/input_test1.json"
+        expected_p1_mov = "W"
+        expected_p1_hit = "P"
+        expected_p2_mov = "D"
+        expected_p2_hit = "K"
+        self.talanaKombat.load_fight_json(file_path)
+
+        self.assertEqual(expected_p1_mov, self.talanaKombat.players[0].movements_list[0])
+        self.assertEqual(expected_p1_hit, self.talanaKombat.players[0].hits_list[0])
+        self.assertEqual(expected_p2_mov, self.talanaKombat.players[1].movements_list[0])
+        self.assertEqual(expected_p2_hit, self.talanaKombat.players[1].hits_list[0])
+
 
 
