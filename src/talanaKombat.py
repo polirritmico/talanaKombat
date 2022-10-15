4a89b8e42d82b0e3b89c9aa8b4bf5d4f0d82d931
polirritmico <ebray187@gmail.com>
2022-10-15 12:12:23 -0300

    Small refaction renamed vars

--- src/talanaKombat.py
+++ src/talanaKombat.py
@@ -6,8 +6,8 @@
 import os
 import json
 
-from config import Players_combo_collection
 from src.player import Player
+from config import Players_combo_collection
 
 class TalanaKombat(object):
     players = []
@@ -23,17 +23,17 @@ class TalanaKombat(object):
     def load_pressed_keys_json(self, filename: str):
         with open(filename, "r") as stream:
             try:
-                fight_file = json.load(stream)
+                keylog = json.load(stream)
             except json.JSONDecodeError as err:
                 print("ERROR: Bad JSON file: ")
                 raise err
         # Parse the json data
-        self.players[0].pressed_moves = fight_file.get("player1").get("movimientos")
-        self.players[1].pressed_moves = fight_file.get("player2").get("movimientos")
-        self.players[0].pressed_attacks = fight_file.get("player1").get("golpes")
-        self.players[1].pressed_attacks = fight_file.get("player2").get("golpes")
+        self.players[0].pressed_moves = keylog.get("player1").get("movimientos")
+        self.players[1].pressed_moves = keylog.get("player2").get("movimientos")
+        self.players[0].pressed_attacks = keylog.get("player1").get("golpes")
+        self.players[1].pressed_attacks = keylog.get("player2").get("golpes")
 
     
-    def run(self, input):
+    def run(self):
         pass
 
