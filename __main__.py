f5d47d0c3e8509121af4c83aa2ab08385038690f
polirritmico <ebray187@gmail.com>
2022-10-15 12:13:09 -0300

    __main__.py load fight_case1.json if no arg passed

--- __main__.py
+++ __main__.py
@@ -7,9 +7,10 @@ from src.talanaKombat import TalanaKombat
 
 if __name__ == "__main__":
     talanaKombat = TalanaKombat()
+    filename = sys.argv[1] if len(sys.argv[1:]) == 1 else "test/fight_case1.json"
     try:
-        #talanaKombat.load_fight_file()
-        sys.exit(talanaKombat.run(sys.argv[1:]))
+        talanaKombat.load_pressed_keys_json(filename)
+        sys.exit(talanaKombat.run())
     except Exception as err:
         print("Catched exception: \n  {}\nClosing TalanaKombat".format(err))
         sys.exit()
