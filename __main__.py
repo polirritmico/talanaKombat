2807f7e6f276016732b76398a60dfbb5da80d764
polirritmico <ebray187@gmail.com>
2022-10-16 10:43:03 -0300

    Remove test from __main__ and add except traceback

--- __main__.py
+++ __main__.py
@@ -3,18 +3,21 @@
 # Eduardo Bray (ejbray@uc.cl)
 
 import sys
+import traceback
 from src.talanaKombat import TalanaKombat
 
 if __name__ == "__main__":
-    talanaKombat = TalanaKombat()
-    filename = sys.argv[1] if len(sys.argv[1:]) == 1 else "test/fight_case3.json"
+    if len(sys.argv) != 2:
+        print("Error: Check usage: ./talanaKombat.py FILENAME")
+        sys.exit()
+    filename = sys.argv[2] #if len(sys.argv[1:]) == 1 else "test/cases/fight_case3.json"
     try:
-        from src.combo import Combo
-        combo = Combo("Toasty", 12, "WSA", "P")
-        keys = "WSAP"
-        output = combo.check_pressed_keys(keys)
-        sys.exit(talanaKombat.run())
-    except Exception as err:
-        print("Catched exception: \n  {}\nClosing TalanaKombat".format(err))
+        talanaKombat = TalanaKombat()
+        talanaKombat.load_pressed_keys_json(filename)
+        talanaKombat.run()
+        sys.exit()
+    except Exception:
+        print("Catched exception: \n  {}".format(traceback.format_exc()))
+        print("Closing TalanaKombat...")
         sys.exit()
 
